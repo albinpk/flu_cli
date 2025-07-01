a589ffcebb0bae41bb43d56e4c9ceb4fe456c8c3
Albin <56157868+albinpk@users.noreply.github.com>
2025-07-01 20:01:18 +0530

    chore: add script for generating version file

---
+++ scripts/generate_version.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:yaml/yaml.dart';
+
+Future<void> main() async {
+  final versionPath = p.joinAll(
+    [Directory.current.path, 'packages', 'flu', 'lib', 'version.g.dart'],
+  );
+  print('Updating generated file $versionPath');
+  final fluPubspec = p.joinAll([
+    Directory.current.path,
+    'packages',
+    'flu',
+    'pubspec.yaml',
+  ]);
+  final yamlMap = loadYaml(await File(fluPubspec).readAsString()) as YamlMap;
+  final currentVersion = yamlMap['version'] as String;
+  final versionFileContent =
+      """
+// This file is generated.
+
+/// The current version of Flu CLI.
+const String fluVersion = '$currentVersion';
+""";
+  await File(versionPath).writeAsString(versionFileContent);
+  print('Version updated successfully');
+}
