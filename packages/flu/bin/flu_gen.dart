f8e6bd9bdf66de701debb8d7a3484adbecc10d7e
Albin <56157868+albinpk@users.noreply.github.com>
2025-07-26 16:39:45 +0530

    fix

--- packages/flu/bin/flu_gen.dart
+++ packages/flu/bin/flu_gen.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:path/path.dart' as path;
 import 'package:path/path.dart';
 import 'package:process_run/process_run.dart';
 
 Future<void> main(List<String> args) async {
   await Shell().run('pwd');
   print('src: ${Platform.script}');
-  print('src: ${dirname(Platform.script.toFilePath())}');
+  final dir = dirname(Platform.script.toFilePath());
+  await File(path.join(dir, 'flu_hello')).create();
+  print('done');
 }
